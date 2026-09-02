3bed5e5a57003355d245fc0c4c429401d0943272
Shiva Jaiswal <jaiswalshiva112233@gmail.com>
2026-09-02 12:54:52 +0530

    feat(tracking): render direct GA4, Google Ads, and Meta Pixel tags in layout head and enable in apphosting.yaml

--- app/layout.tsx
+++ app/layout.tsx
@@ -284,6 +284,64 @@ export default async function RootLayout({
             content={trackingSettings.bingWebmasterVerification}
           />
         ) : null}
+
+        {trackingSettings.analyticsEnabled && trackingSettings.googleAnalyticsId ? (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(
+                trackingSettings.googleAnalyticsId,
+              )}`}
+            />
+            <script
+              id="google-analytics-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${trackingSettings.googleAnalyticsId}', {
+                    page_path: window.location.pathname,
+                    anonymize_ip: true
+                  });
+                `,
+              }}
+            />
+          </>
+        ) : null}
+
+        {trackingSettings.advertisingEnabled && trackingSettings.googleAdsId ? (
+          <script
+            id="google-ads-init"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('config', '${trackingSettings.googleAdsId}');
+              `,
+            }}
+          />
+        ) : null}
+
+        {trackingSettings.metaPixelEnabled && trackingSettings.metaPixelId ? (
+          <script
+            id="meta-pixel-init"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${trackingSettings.metaPixelId}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        ) : null}
       </head>
 
       <body className="antialiased">
